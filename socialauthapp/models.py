8ee3b1d26e937043fe62a51bb9b3cb5bdf671e4e
Vaclav Klecanda <vencax77@gmail.com>
2012-05-14 15:40:16 +0200

    obsolete model removed cs translations loginbox template

--- socialauthapp/models.py
+++ socialauthapp/models.py
@@ -1,19 +1,2 @@
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-#    class Meta:
-#        verbose_name = _('event')
-#        verbose_name_plural = _('events')
-#        ordering = ['-event_begin', '-title']
-#        get_latest_by = 'event_begin'
-#        permissions = (("change_author", ugettext("Change author")),)
-#        unique_together = ("event_begin", "title")
-
-    def __unicode__(self):
-        return _('Profile of %s') % self.user
-
-    user = models.ForeignKey(User)
-    title = models.CharField(_('title'), max_length=255)
 
